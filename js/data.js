caf94bfe850a9175044f2658ecd0168a602b6af9
unknown <>
2016-07-14 11:39:00 +0800

    Signed-off-by: unknown <大胃>

---
+++ js/data.js
@@ -0,0 +1,51 @@
+var users = [{
+	uname: "玩家一",
+	id: null,
+	win: 0,
+	total: 0,
+	status: 1
+},{
+	uname: "玩家二",
+	id: null,
+	win: 0,
+	total: 0,
+	status: 2
+},{
+	uname: "玩家三",
+	id: null,
+	win: 0,
+	total: 0,
+	status: 3
+},{
+	uname: "玩家二",
+	id: null,
+	win: 0,
+	total: 0,
+	status: 2
+},{
+	uname: "玩家三",
+	id: null,
+	win: 0,
+	total: 0,
+	status: 3
+},{
+	uname: "玩家二",
+	id: null,
+	win: 0,
+	total: 0,
+	status: 1
+},{
+	uname: "玩家三",
+	id: null,
+	win: 0,
+	total: 0,
+	status: 2
+}]
+var room = [
+{roomname:'01'},
+{roomname:'02'},
+{roomname:'03'},
+{roomname:'04'},
+{roomname:'05'},
+{roomname:'06'}
+]
